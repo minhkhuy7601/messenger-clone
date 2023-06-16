34f2a4647a7f05bd06b574010ee576a5277f969c
minhkhuy <khuy.tran@roboworld.io>
2023-06-16 15:44:31 +0700

    Add auth form

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-    serverComponentsExternalPackages: ["next-auth"],
-  },
+  // experimental: {
+  //   appDir: true,
+  //   serverComponentsExternalPackages: ["next-auth"],
+  // },
   images: {
     domains: [
       "res.cloudinary.com",
